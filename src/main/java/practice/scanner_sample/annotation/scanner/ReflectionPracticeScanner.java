226ef4a0481d9af708229628799a4c2cde67126e
mothership2002 <mothership2002@naver.com>
2023-06-17 23:10:10 +0900

    no message

--- src/main/java/practice/scanner_sample/annotation/scanner/ReflectionPracticeScanner.java
+++ src/main/java/practice/scanner_sample/annotation/scanner/ReflectionPracticeScanner.java
@@ -1,26 +1,27 @@
 package practice.scanner_sample.annotation.scanner;
 
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import practice.scanner_sample.annotation.ReflectionPractice;
 import practice.scanner_sample.controller.ReflectionController;
 
 import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
-@Slf4j
+@RequiredArgsConstructor
 public class ReflectionPracticeScanner {
 
     private final Map<String, Method> methodMap = new HashMap<>();
+    private final ReflectionController controller;
 
     @PostConstruct
     public void scanningAnnotation() {
-        Method[] methods = ReflectionController.class.getDeclaredMethods();
+        Method[] methods = controller.getClass().getDeclaredMethods();
         Arrays.stream(methods)
                 .filter(method -> method.getDeclaredAnnotation(ReflectionPractice.class) != null)
                 .forEach(method -> methodMap.put(method.getDeclaredAnnotation(ReflectionPractice.class).value(), method));
@@ -34,4 +35,11 @@ public class ReflectionPracticeScanner {
         return methodMap.size();
     }
 
+    public Object invokeMethod(String key, Object component) throws InvocationTargetException, IllegalAccessException {
+        return invokeMethod(key, component, null);
+    }
+
+    public Object invokeMethod(String key, Object component, Object... arg) throws InvocationTargetException, IllegalAccessException {
+        return methodMap.get(key).invoke(component, arg);
+    }
 }
