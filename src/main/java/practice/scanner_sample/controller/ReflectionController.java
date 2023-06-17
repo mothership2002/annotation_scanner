fe4ac0ab1915ab29c569a6d7e0aeda7a21c843e4
mothership2002 <mothership2002@naver.com>
2023-06-17 16:28:41 +0900

    no message

--- src/main/java/practice/scanner_sample/controller/ReflectionController.java
+++ src/main/java/practice/scanner_sample/controller/ReflectionController.java
@@ -1,15 +1,18 @@
 package practice.scanner_sample.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import practice.scanner_sample.annotation.ReflectionPractice;
 
-//@Component
+@Component
+@Slf4j
 public class ReflectionController {
 
     @ReflectionPractice("/hello")
-    public void methodOne() {
-
+    public Long methodOne() {
+        log.info("Hello World");
+        return 1L;
     }
 
     @ReflectionPractice("/world")
--- src/test/java/practice/scanner_sample/AnnotationScanningTest.java
+++ src/test/java/practice/scanner_sample/AnnotationScanningTest.java
@@ -1,5 +1,6 @@
 package practice.scanner_sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import practice.scanner_sample.annotation.ReflectionPractice;
 import practice.scanner_sample.annotation.scanner.ReflectionPracticeScanner;
 import practice.scanner_sample.controller.ReflectionController;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
@@ -17,21 +19,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Slf4j
-public class AnnotationTest {
+public class AnnotationScanningTest {
+
 
     @Autowired
-    ReflectionPracticeScanner reflectionPracticeScanner;
+    private ReflectionController reflectionController;
+
+    @Autowired
+    private ReflectionPracticeScanner reflectionPracticeScanner;
+
+    private final Class<ReflectionController> controllerClass = ReflectionController.class;
+    private final Class<ReflectionPractice> annotationClass = ReflectionPractice.class;
 
     @Test
     void checkMapData() {
         Map<String, Method> methodMap = reflectionPracticeScanner.getMethodMap();
-        int methodCount = (int) Arrays.stream(ReflectionController.class.getMethods())
-                .filter(method -> method.getDeclaredAnnotation(ReflectionPractice.class) != null)
+        int methodCount = (int) Arrays.stream(controllerClass.getMethods())
+                .filter(method -> method.getDeclaredAnnotation(annotationClass) != null)
                 .count();
         int methodMapSize = methodMap.size();
 
 //        methodMap.forEach((k,v) -> log.info("key = {}, value = {}", k, v.getName()));
         assertThat(methodCount).isEqualTo(methodMapSize);
 
+    }
+
+    @Test
+    void getMapMethod() throws InvocationTargetException, IllegalAccessException {
+        Map<String, Method> methodMap = reflectionPracticeScanner.getMethodMap();
+
+        Method method = methodMap.get("/hello");
+        Long returnType = (Long) method.invoke(reflectionController);
+        assertThat(returnType).isEqualTo(1L);
     }
 }
